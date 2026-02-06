d88885c4437ab1705c9937819cc9e8ece289cf7d
amjad-haider <amjadckhaid@gmail.com>
2026-02-06 12:50:22 +0100

    publications updated

--- portfolio-website/src/app/components/publications/publications.ts
+++ portfolio-website/src/app/components/publications/publications.ts
@@ -19,22 +19,29 @@ export class Publications {
   // Define your publication entries
   publications = [
     {
-      title: 'Title of Research Paper One: Analyzing Data Structures in C++',
-      authors: 'A. Haider, J. Smith',
-      journal: 'International Journal of Algorithms',
-      year: 2023,
-      doi: '10.xxxx/xxxx-xxxx-xxxx',
-      link: 'https://doi.org/your/doi/link' // Link to the paper's source/PDF
+      title: 'COMponent-Aware Pruning for Accelerated Control Tasks in Latent Space Models',
+      authors: 'G. Sundaram, J. Ulmen, A. Haider, D. Görges',
+      journal: 'The 2026 IEEE/SICE International Symposium on System Integration (SII 2026)',
+      year: 2025,
+      doi: '10.48550/arXiv.2508.08144',
+      link: 'https://doi.org/10.48550/arXiv.2508.08144' // Link to the paper's source/PDF
     },
     {
-      title: 'Optimizing Python Scripts for Web Scraping using Async Methods',
-      authors: 'A. Haider',
-      journal: 'Conference on Web Development',
-      year: 2024,
-      doi: null,
-      link: 'https://www.example.com/paper-two' 
+      title: 'Application-Specific Component-Aware Structured Pruning of Deep Neural Networks via Soft Coefficient Optimization',
+      authors: 'G. Sundaram, J. Ulmen, A. Haider, D. Görges',
+      journal: '24th European Control Conference (ECC26)',
+      year: 2025,
+      doi: '10.48550/arXiv.2507.14882',
+      link:  'https://doi.org/10.48550/arXiv.2507.14882',
+    },
+    {
+      title: 'Denoising and Segmentation of SONAR Images for Rescue Operations',
+      authors: 'H. E. Keen, A. Haider, K. Berns',
+      journal: 'ISR Europe 2023; 56th International Symposium on Robotics',
+      year: 2023,
+      doi: 'ISBN:978-3-8007-6140-1',
+      link:  'https://ieeexplore.ieee.org/abstract/document/10363104',
     }
-    // Add more publications here
   ];
 
 }
--- portfolio-website/src/app/components/sidebar/sidebar.ts
+++ portfolio-website/src/app/components/sidebar/sidebar.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
+  imports: [CommonModule],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css']
 })
